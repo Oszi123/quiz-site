f80b1feb8081e7de55e7a9aab84259ecaf0f5acc
osb1996 <oscar.bonilla123@outlook.com>
2022-07-07 11:45:53 -0400

    Javascript and CSS added

---
+++ script.js
@@ -0,0 +1,16 @@
+var answers = document.querySelector("#answers");
+
+var scores = document.querySelector("#highscore")
+
+var startQuizBtn = document.querySelector("#start-quiz-btn")
+
+var answerOne = document.querySelector("#answer-one")
+var answerTwo = document.querySelector("#answer-two")
+var answerThree = document.querySelector("#answer-three")
+var answerFour = document.querySelector("#answer-four")
+
+var submit = document.querySelector("#submit")
+
+generateBtn.addEventListener("click", writePassword);
+
+function
